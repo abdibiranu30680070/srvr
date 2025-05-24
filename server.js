afd70c06358848236559da3459299449a56088d3
abdibiranu30680070 <153322769+abdibiranu30680070@users.noreply.github.com>
2025-05-25 00:09:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,18 +23,9 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-// Server initialization
-<<<<<<< HEAD
-const port = process.env.PORT || 3001; // Using environment variable for the port
-app.listen(port, () => {
-    console.log(`Server started successfully on port ${port}`);
-});
-=======
+
 const port = process.env.PORT || 4001; // Using environment variable for the port
 app.listen(port, () => {
     console.log(`Server started successfully on port ${port}`);
 });
-const authRoutes = require("./Routes/auth");
-app.use("/api/auth", authRoutes);
 
->>>>>>> 1845fcf (Initial commit)
